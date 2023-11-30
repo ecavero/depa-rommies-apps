87ea2276a21631363e66b63510e91a4a1bf2d704
yasser <yassermorenor@gmail.com>
2023-11-30 00:01:44 -0500

    Creación de Pantalla Usuario

--- app/src/main/java/com/example/deparommies/MainActivity.java
+++ app/src/main/java/com/example/deparommies/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.deparommies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText usuario, contrasena;
+    Button ingresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usuario=findViewById(R.id.edtUsuario);
+        contrasena=findViewById(R.id.edtContrasena);
+        ingresar=findViewById(R.id.edtContrasena);
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/deparommies/Usuario.java
@@ -0,0 +1,55 @@
+package com.example.deparommies;
+
+public class Usuario {
+
+    private String nombre, apellido, descripcion;
+    private int edad, foto;
+
+    public Usuario(String nombre, String apellido, String descripcion, int edad, int foto) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.descripcion = descripcion;
+        this.edad = edad;
+        this.foto = foto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+}
