6f8a8e66b29560c47a5854813d5345319bcc4a95
Eduardo Cavero <ecavero@gmail.com>
2023-12-01 22:27:16 -0500

    Avance de pantalla de Departamentos

--- app/src/main/java/com/example/deparommies/Departamento.java
+++ app/src/main/java/com/example/deparommies/Departamento.java
@@ -1,4 +1,47 @@
 package com.example.deparommies;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Departamento {
+    private String nombre;
+    private String descripcion;
+    private int fotoPrincipal;
+    private List<Integer> fotosAdicionales = new ArrayList<>();
+
+    public Departamento(String nombre, String descripcion, int fotoPrincipal) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.fotoPrincipal = fotoPrincipal;
+    }
+
+    public void agregarFotosAdicionales(Integer ... fotosParaAgregar) {
+        List<Integer> listaFotos = Arrays.asList(fotosParaAgregar);
+        fotosAdicionales.addAll(listaFotos);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getFotoPrincipal() {
+        return fotoPrincipal;
+    }
+
+    public void setFotoPrincipal(int fotoPrincipal) {
+        this.fotoPrincipal = fotoPrincipal;
+    }
 }
---
+++ app/src/main/java/com/example/deparommies/DepartamentoAdapter.java
@@ -0,0 +1,44 @@
+package com.example.deparommies;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class DepartamentoAdapter extends RecyclerView.Adapter {
+
+    private List<Departamento> listaDepartamentos;
+
+    public DepartamentoAdapter(List<Departamento> listaDepartamentos) {
+        this.listaDepartamentos = listaDepartamentos;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View layoutItem = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.departamentos_item, viewGroup, false);
+        DepartamentoViewHolder viewHolder = new DepartamentoViewHolder(layoutItem);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+        Departamento departamento = listaDepartamentos.get(i);
+        DepartamentoViewHolder departamentoViewHolder = (DepartamentoViewHolder) viewHolder;
+        departamentoViewHolder.cbSeleccion.setSelected(false);
+        departamentoViewHolder.txtNombre.setText(departamento.getNombre());
+        departamentoViewHolder.imgFotoPrincipal.setImageResource(departamento.getFotoPrincipal());
+        departamentoViewHolder.txtDescripcion.setText(departamento.getDescripcion());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaDepartamentos.size();
+    }
+}
